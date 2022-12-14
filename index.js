628471acaa324aff757c928342773aae5bc74e7a
guantai <jpguantai@gmail.com>
2022-12-14 14:32:11 +0300

    added

--- index.js
+++ index.js
@@ -2,37 +2,33 @@ document.addEventListener('DOMContentLoaded', () => {
     getFood();
 });
 
-
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '1899fde54bmsh5de83a52e80fa69p1b73ccjsn1a72bda5310e',
-		'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+		'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
 	}
 };
 
 const getFood = () => {
-fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/informationBulk?ids=456%2C987%2C321', options)
+fetch('https://edamam-recipe-search.p.rapidapi.com/search?q=chicken', options)
 	.then(response => response.json())
 	.then(data => {
         renderFood(data);
-    }
-    )
+    })
 	.catch(err => console.error(err));
 };
 
 function renderFood(data) {
-    const foodContainer = document.getElementById('food-container');
-    data.forEach(food => {
-        const foodDiv = document.createElement('div');
-        foodDiv.className = 'food';
-        foodDiv.innerHTML = `
-            <h2>${food.title}</h2>
-            <img src="${food.image}">
-            <p>${food.summary}</p>
+    const foodList = document.querySelector('#food-list');
+    data.hits.forEach((food) => {
+        const foodItem = document.createElement('li');
+        foodItem.innerHTML = `
+            <img src="${food.recipe.image}" alt="${food.recipe.label}">
+            <h2>${food.recipe.label}</h2>
+            <p>${food.recipe.source}</p>
+            <a href="${food.recipe.url}">View Recipe</a>
         `;
-        foodContainer.appendChild(foodDiv);
-    }
-    )
+        foodList.appendChild(foodItem);
+    });
 }
-
